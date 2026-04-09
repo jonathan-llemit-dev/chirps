4e4bce16d2b061bf69593191d5e5914532f21c99
Jonathan Llemit <jonathan@clearmindai.com>
2026-04-09 15:02:20 +0800

    Add migrations for comments and likes

---
+++ database/migrations/2026_04_09_061527_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chirp_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('message', 255);
+            $table->timestamps();
+
+            $table->index(['chirp_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
